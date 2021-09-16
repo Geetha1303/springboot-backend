c7952db111eb5218f1e169961b07d0cd6bce315f
Geetha1303 <geetha@zelarsoft.com>
2021-09-16 18:56:45 +0530

    update

--- springboot/src/main/java/net/guides/springboot/crud/controller/EmployeeController.java
+++ springboot/src/main/java/net/guides/springboot/crud/controller/EmployeeController.java
@@ -24,13 +24,14 @@ import net.guides.springboot.crud.repository.EmployeeRepository;
 import net.guides.springboot.crud.service.SequenceGeneratorService;
 
 //@CrossOrigin(origins = "http://3.234.141.206:3000")
-@CrossOrigin(origins = "http://18.213.245.199:3000")
+@CrossOrigin(origins = "http://react-frontend-service:3000")
 @RestController
 @RequestMapping("/api/v1")
 public class EmployeeController {
 	@Autowired
 	private EmployeeRepository employeeRepository;
-	
+
+
 	@Autowired
 	private SequenceGeneratorService sequenceGeneratorService;
 
